689ceecc7bfce1e100640f2f0ed60e61e29fbba7
Nikita Galaganov <nickgalaganov@gmail.com>
2020-01-31 14:23:47 +0100

    Initial Commit

---
+++ DadJoke/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJoke
+//
+//  Created by Nikita Galaganov on 31.01.2020.
+//  Copyright © 2020 Nikita Galaganov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
